8d2070019066e208dd17177110c5020b0f24d2a7
NuttWarunyu <NuttWarunyu@gmail.com>
2025-04-29 08:19:12 +0700

    ♻️ Fix CORS: allow frontend Railway URL

--- app/main.py
+++ app/main.py
@@ -13,7 +13,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173", 
+    "https://plantpick-frontend.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
